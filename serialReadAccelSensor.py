23c511309aca54a988a7791196a542edd4647185
Dilini Nissanka <diliniss@cs.washington.edu>
2023-07-13 11:11:19 -0700

    Collected accel data and plotted axis

---
+++ serialReadAccelSensor.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import serial
+
+serialInst = serial.Serial()
+serialInst.baudrate = 115200
+serialInst.port = 'COM5'
+
+def main():
+    readSerial()
+
+def readSerial():
+    file_name = input('Please enter file name:')
+    file_name = file_name + '.txt'
+    serialInst.open()
+    output_list = []
+    try:
+        while(serialInst.readable()):
+            packet = serialInst.readline()
+            output_list.append(packet.decode('utf').rstrip() + "\n")
+    except (Exception, KeyboardInterrupt):
+        pass
+
+    serialInst.close()
+
+    with open(file_name,"w") as file:
+        file.writelines(output_list)
+    
+main()
